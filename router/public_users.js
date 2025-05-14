30287eb9c58471b050c4757c241c410a12886afd
Abed <abedkansof18@gmail.com>
2025-05-14 08:39:02 +0300

    Last Commit

--- router/public_users.js
+++ router/public_users.js
@@ -3,43 +3,45 @@ const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const { serialize } = require('cookie');
 const public_user = express.Router();
+// const { serialize } = require('cookie');
 // const { loginLimiter } = require('../middleware/ratelimiter');
 
 
 public_user.post('/login', async (req, res) => {
-    const { email, password, rememberMe } = req.body;
+    try {
+        const { email, password, rememberMe } = req.body;
 
-    if (!email || !password) 
-        return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email and password required' });
+        if (!email || !password) 
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email and password required' });
 
-    const user = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
-    console.log(user);
+        if (!user) 
+            return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
 
-    if (!user) 
-        return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
-    
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+            return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch){
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
+        const token = jwt.sign({ id: user._id, email: user.email }, 'access', {
+            expiresIn: rememberMe ? '2h' : '30m'
+        });
 
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: rememberMe ? 2 * 60 * 60 * 1000 : 30 * 60 * 1000
+        });
+
+        res.status(StatusCodes.OK).json({ message: 'Login successful' });
+
+    } catch (err) {
+        console.error('Login error:', err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
     }
-        
-    const token = jwt.sign({ id: user._id, email: user.email }, 'access', {
-        expiresIn: rememberMe ? '2h' : '30m'
-    });
-
-    res.cookie('token', token, {
-        httpOnly: true,
-        secure: true, 
-        sameSite: 'lax',
-        maxAge: rememberMe ? 2 * 60 * 60 * 1000 : 30 * 60 * 1000 // 2 hours with remember_me, else 30 minutes
-    });
-
-    res.status(StatusCodes.OK).json({ message: 'Login successful' });
 });
+
 
 module.exports = public_user;
